9a8756141980d7b67a62940aa96c6edd0f9b77b5
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-04 12:07:49 +0900

    ✏️ edit : Provider.getTimeline 로직 업데이트

--- Mukku/MukkuWidgets/HomeWidgets/Provider.swift
+++ Mukku/MukkuWidgets/HomeWidgets/Provider.swift
@@ -1,12 +1,14 @@
-import WidgetKit
 import SwiftUI
+import WidgetKit
 import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
+        //return SimpleEntry()
         SimpleEntry()
     }
 
+    // Config 설정가능한 곳
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), scene: "mask", configuration: configuration, count: 1)
         completion(entry)
@@ -14,16 +16,35 @@ struct Provider: IntentTimelineProvider {
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> ()) {
         var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, scene: "mask", configuration: ConfigurationIntent(), count: 1)
+        
+        // 이미지 갯수 세기
+        let topic =  matching(by: configuration.mc2Enum)
+        let imgNum = countImgNum(name : topic)
+        
+        //120 개에 가까운 img 생성
+        for imgCount in 0 ..< (120/imgNum)*imgNum {
+            let entryDate = Calendar.current.date(byAdding: .second, value: imgCount, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, scene: topic, configuration: ConfigurationIntent(), count: (imgCount%imgNum+1))
             entries.append(entry)
         }
+        //print("Done in \(Date())")
+        //print(entries.count) //이건 매번 갱신됨, 항상 120개가 되는 것을 알 수 있다.
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+        let timeline = Timeline(entries: entries, policy: .after(.now.advanced(by: 120)))
         completion(timeline)
+    }
+    fileprivate func countImgNum(name imgName : String)->Int{
+        for i in 1..<10 {
+            if let _ = UIImage(named:"\(imgName)\(i)") {continue} else {
+                return i-1}}
+        return 0
+    }
+    fileprivate func matching(by mC2WidgetEnum: Mc2Enum)->String{
+        switch mC2WidgetEnum {
+        case.phantomOfOpera1: return "mask"
+        case.phantomOfOpera2: return "boat"
+        case.unknown: return "mask"
+        }
     }
 }
