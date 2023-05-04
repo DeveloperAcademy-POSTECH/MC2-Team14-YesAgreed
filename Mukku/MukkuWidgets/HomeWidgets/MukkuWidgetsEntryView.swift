082cdeb0565923d3fd87b23244ba1ff95c8cfed2
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-04 12:26:04 +0900

    ✏️ edit : MukkuWidgetsEntryView 수정

--- Mukku/MukkuWidgets/HomeWidgets/MukkuWidgetsEntryView.swift
+++ Mukku/MukkuWidgets/HomeWidgets/MukkuWidgetsEntryView.swift
@@ -4,9 +4,15 @@ import Intents
 
 struct MukkuWidgetsEntryView : View {
     var entry: SimpleEntry
-
     var body: some View {
-        Text(entry.date, style: .time)
+        switch entry.scene{
+        case "mask":
+            PhantomOfOperaMask(count: entry.count)
+        case "boat":
+            PhantomOfOperaBoat(count: entry.count)
+        default:
+            PhantomOfOperaMask(count: entry.count)
+        }
     }
 }
 
