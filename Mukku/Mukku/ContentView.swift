c988cc59bc8c2ba15545b5b8db131f47226658aa
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-04 12:50:27 +0900

    ✨ feat : 앱과 다이나믹 아일랜드 동기화 기능 구현

--- Mukku/Mukku/ContentView.swift
+++ Mukku/Mukku/ContentView.swift
@@ -1,21 +1,49 @@
-//
-//  ContentView.swift
-//  Mukku
-//
-//  Created by Kihyun Roh on 2023/05/04.
-//
-
 import SwiftUI
+import ActivityKit
 
 struct ContentView: View {
+    @State private var isTrackingTime : Bool = false
+    @State private var activity : Activity<MukkuWidgetsAttributes>? = nil
+    @State private var startTime : Date? = nil
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack{
+                if let startTime {
+                    Text(startTime, style: .relative)
+                }
+                Button {
+                    isTrackingTime.toggle()
+                    
+                    if isTrackingTime {
+                        startTime = .now
+                        
+                        //Start the live Activity
+                        let attributes = MukkuWidgetsAttributes()
+                        let state = MukkuWidgetsAttributes.ContentState(startTime: .now)
+                        
+                        activity = try? Activity<MukkuWidgetsAttributes>.request(
+                            attributes: attributes,
+                            contentState: state)
+                    } else {
+                        //End the live Activity
+                        guard let startTime else{return}
+                        let state = MukkuWidgetsAttributes.ContentState(startTime: startTime)
+    
+                        Task{
+                            await activity?.end(using: state, dismissalPolicy: .immediate)
+                        }
+            
+                        self.startTime = nil
+                        
+                    }
+                    
+                } label : {
+                    Text(isTrackingTime ? "Stop" : "Start")
+                    
+                }
+                .navigationTitle("Basic Time Tracker")
+            }
         }
-        .padding()
     }
 }
 
