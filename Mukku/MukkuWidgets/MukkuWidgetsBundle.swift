94442f156e592ab9dbec203ce4aa00bccb6b6c2a
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-04 11:13:21 +0900

    🎉 init : 위젯 생성

---
+++ Mukku/MukkuWidgets/MukkuWidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MukkuWidgetsBundle.swift
+//  MukkuWidgets
+//
+//  Created by Kihyun Roh on 2023/05/04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MukkuWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        MukkuWidgets()
+        MukkuWidgetsLiveActivity()
+    }
+}
