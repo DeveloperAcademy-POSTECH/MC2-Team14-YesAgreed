b0a0c29931a08f8c232bbfbeb79936e542d3d1a5
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-04 11:34:16 +0900

    🗃️ model : SimpleEntry 모델 생성

--- Mukku/MukkuWidgets/HomeWidgets/SimpleEntry.swift
+++ Mukku/MukkuWidgets/HomeWidgets/SimpleEntry.swift
@@ -1,18 +1,28 @@
-import WidgetKit
 import SwiftUI
-import Intents
+import WidgetKit
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
+    let date: Date      // 날짜
+    let scene: String   //Scene 장면
     let configuration: ConfigurationIntent
+    let count: Int      //몇번째 이미지인지 체크해야함
     
     init() {
         self.date = Date()
+        self.scene = "mask"
         self.configuration = ConfigurationIntent()
+        self.count = 1
     }
-    
-    init(date: Date, configuration: ConfigurationIntent){
+    init(scene:String){
+        self.date = Date()
+        self.scene = scene
+        self.configuration = ConfigurationIntent()
+        self.count = 1
+    }
+    init(date: Date, scene : String, configuration: ConfigurationIntent, count: Int) {
         self.date = date
+        self.scene = scene
         self.configuration = configuration
+        self.count = count
     }
 }
