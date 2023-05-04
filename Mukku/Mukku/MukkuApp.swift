dac2bac6876b2d916c2d11bd08dbf12f5e57d77d
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-04 11:12:21 +0900

    🎉 init : 프로젝트 생성

---
+++ Mukku/Mukku/MukkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MukkuApp.swift
+//  Mukku
+//
+//  Created by Kihyun Roh on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct MukkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
