80fdbc2b1a64611d3a3e1cb6dec9d3f49e985227
Jae Ho Yoon (Marcus) <mr.jaeho@gmail.com>
2023-05-09 15:24:34 +0900

    💄 style : 워치 La Mancha의 RenderingMode 별 색상 변경

--- Mukku/MukkuWatchExtension/Views_Watch/LaManchaTypo.swift
+++ Mukku/MukkuWatchExtension/Views_Watch/LaManchaTypo.swift
@@ -5,28 +5,53 @@ struct LaManchaTypo: View {
     
     @Environment(\.widgetFamily) var widgetFamily
     @Environment(\.widgetRenderingMode) var widgetRenderingMode
+    
     var body: some View {
         
         switch widgetFamily {
-            
         case .accessoryCircular:
-            Image("typo_watch")
-                .resizable()
-                .scaledToFit()
-                .widgetAccentable(true)
-                .unredacted()
-            
-        case .accessoryRectangular:
-            HStack {
+            switch widgetRenderingMode {
+            case .fullColor:
+                Image("typo_watch_full")
+                    .resizable()
+                    .scaledToFit()
+                    .widgetAccentable(true)
+                    .unredacted()
+            default:
                 Image("typo_watch")
                     .resizable()
                     .scaledToFit()
                     .widgetAccentable(true)
-                Text("The Impossible Dream")
-                Spacer()
+                    .unredacted()
             }
-            .padding(.leading, 6)
-            .unredacted()
+            
+        case .accessoryRectangular:
+            switch widgetRenderingMode {
+            case .fullColor:
+                HStack {
+                    Image("typo_watch_full")
+                        .resizable()
+                        .scaledToFit()
+                        .widgetAccentable(true)
+                        .padding(4)
+                    Text("The Impossible Dream")
+                    Spacer()
+                }
+                .padding(.leading, 6)
+                .unredacted()
+            default:
+                HStack {
+                    Image("typo_watch")
+                        .resizable()
+                        .scaledToFit()
+                        .widgetAccentable(true)
+                    Text("The Impossible Dream")
+                    Spacer()
+                }
+                .padding(.leading, 6)
+                .unredacted()
+            }
+            
             
         case .accessoryInline:
             Text("The Impossible Dream")
@@ -34,14 +59,27 @@ struct LaManchaTypo: View {
                 .unredacted()
             
         case .accessoryCorner:
-            Image("typo_watch")
-                .resizable()
-                .scaledToFit()
-                .widgetLabel {
-                    Text("The Impossible Dream")
-                        .widgetAccentable(true)
-                }
-                .unredacted()
+            switch widgetRenderingMode {
+            case .fullColor:
+                Image("typo_watch_full")
+                    .resizable()
+                    .scaledToFit()
+                    .widgetLabel {
+                        Text("가네 저 별을 향하여")
+                            .widgetAccentable(true)
+                    }
+                    .unredacted()
+            default:
+                Image("typo_watch")
+                    .resizable()
+                    .scaledToFit()
+                    .widgetLabel {
+                        Text("가네 저 별을 향하여")
+                            .widgetAccentable(true)
+                    }
+                    .unredacted()
+            }
+            
             
         default:
             Text("Widget Not Supported)")
