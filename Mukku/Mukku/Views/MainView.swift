6c86394ccfe6df1505c7da54bb05c6e5a8e09578
seohmoon <dream222ing@gmail.com>
2023-05-12 22:13:15 +0900

    🐛 fix : 위젯 설명 텍스트-> 라벨로 수정

--- Mukku/Mukku/Views/MainView.swift
+++ Mukku/Mukku/Views/MainView.swift
@@ -86,25 +86,25 @@ struct MainView: View {
                         }
                     }
             }
-            Section() {
-                ZStack{
-                    HStack{
-                        Image(systemName: "info.circle")
-                            .foregroundColor(.blue)
-                        Spacer()
-                    }
-                    HStack{
-                        Text("      Widget Instruction")
-                            .foregroundColor(.blue)
-                        Spacer()
+            // 위젯 설정 방법
+                        Section() {
+                            Label {
+                                Text("Widget Instruction")
+                                    .padding(EdgeInsets(top: 0, leading: -20, bottom: 0, trailing: 0))
+                            } icon: {
+                                Image(systemName: "info.circle")
+                                    .padding(EdgeInsets(top: 0, leading: -15, bottom: 0, trailing: 0))
+                            }
+                                .alignmentGuide(.listRowSeparatorLeading) {
+                                    ViewDimensions in
+                                    return -20
+                                }
+                            Text("You can add the Home screen widget and the Lock screen widget directly with a long press gesture on the screen. You can select objects and backgrounds in the Edit Widgets window.")
+                                .font(.system(size: 15))
+                        }
                     }
                 }
-                Text("You can add the Home screen widget and the Lock screen widget directly with a long press gesture on the screen. You can select objects and backgrounds in the Edit Widgets window.")
-                    .font(.system(size: 15))
             }
-        }
-    }
-}
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
