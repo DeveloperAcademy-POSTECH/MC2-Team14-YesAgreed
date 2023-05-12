9f7ac5694ea0c398769e282caf6b89eb7fecabc2
seohmoon <dream222ing@gmail.com>
2023-05-12 22:21:18 +0900

    ♻️ refactor : 불필요한 라인 삭제

--- Mukku/Mukku/Views/MainView.swift
+++ Mukku/Mukku/Views/MainView.swift
@@ -1,10 +1,3 @@
-//
-//  MainView.swift
-//  Mukku
-//
-//  Created by 김혜린 on 2023/05/09.
-//
-
 import SwiftUI
 import ActivityKit
 
@@ -22,7 +15,6 @@ struct MainView: View {
     
     var body: some View {
         Form {
-            
             Section (header:
                         VStack {
                 ForEach(MusicalModel.musicalModels, id:\.self){ musical in
@@ -42,15 +34,12 @@ struct MainView: View {
                                 // Dynamic island update
                                 let updatedState = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
                                 let updatedContent = ActivityContent(state: updatedState, staleDate: .now.advanced(by: 1800.0))
-                                
                                 Task {
                                     await activity?.update(updatedContent)
                                 }
-                                
                             } label: {
                                 ObjectView(isSelected : dynamicIslandScene == musical.title + scene.name, text: scene.name, imageName: scene.icon)}
                             .padding(.bottom, 20)
-                            
                         }
                     }
                 }
@@ -82,29 +71,28 @@ struct MainView: View {
                             let content = ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
                             Task{await activity?.end(content, dismissalPolicy: .immediate)}
                             self.startTime = nil
-                            
                         }
                     }
             }
             // 위젯 설정 방법
-                        Section() {
-                            Label {
-                                Text("Widget Instruction")
-                                    .padding(EdgeInsets(top: 0, leading: -20, bottom: 0, trailing: 0))
-                            } icon: {
-                                Image(systemName: "info.circle")
-                                    .padding(EdgeInsets(top: 0, leading: -15, bottom: 0, trailing: 0))
-                            }
-                                .alignmentGuide(.listRowSeparatorLeading) {
-                                    ViewDimensions in
-                                    return -20
-                                }
-                            Text("You can add the Home screen widget and the Lock screen widget directly with a long press gesture on the screen. You can select objects and backgrounds in the Edit Widgets window.")
-                                .font(.system(size: 15))
-                        }
-                    }
+            Section() {
+                Label {
+                    Text("Widget Instruction")
+                        .padding(EdgeInsets(top: 0, leading: -20, bottom: 0, trailing: 0))
+                } icon: {
+                    Image(systemName: "info.circle")
+                        .padding(EdgeInsets(top: 0, leading: -15, bottom: 0, trailing: 0))
                 }
+                    .alignmentGuide(.listRowSeparatorLeading) {
+                        ViewDimensions in
+                        return -20
+                    }
+                Text("You can add the Home screen widget and the Lock screen widget directly with a long press gesture on the screen. You can select objects and backgrounds in the Edit Widgets window.")
+                    .font(.system(size: 15))
             }
+        }
+    }
+}
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
