abf9f4b0e9e6d30344e25be4433a52119d417e74
dmajorcode <dianekang93@gmail.com>
2023-05-16 13:27:55 +0900

    ✏️ edit : 코드 정리

--- Mukku/Mukku/Utils/Helper.swift
+++ Mukku/Mukku/Utils/Helper.swift
@@ -16,24 +16,24 @@ struct Helper {
     
     static func getImageIdsFromUserDefault() -> [String] {
         if let userDefaults = UserDefaults(suiteName: appGroupName) {
-            print("===========================================")
-            print("in Helper function")
+//            print("===========================================")
+//            print("in Helper function")
             
 //            print("userDefaults:", userDefaults)
 //            print("userDefaults data2:", userDefaults.data(forKey: "29F53808-0302-43BD-AEF6-420A6482B226"))
 //
-            if userDefaults.data(forKey: userDefaultsPhotosKey) != nil{
-                print("userDefaults data5:", userDefaults.data(forKey: userDefaultsPhotosKey)!)
-            } else {
-                print("data is nil")
-            }
-            print(userDefaults.data(forKey: userDefaultsPhotosKey))
+//            if userDefaults.data(forKey: userDefaultsPhotosKey) != nil{
+//                print("userDefaults data5:", userDefaults.data(forKey: userDefaultsPhotosKey)!)
+//            } else {
+//                print("data is nil")
+//            }
+//            print(userDefaults.data(forKey: userDefaultsPhotosKey))
             if let data = userDefaults.data(forKey: userDefaultsPhotosKey) {
-                print("data:", data)
+//                print("data:", data)
                 
                 return try! JSONDecoder().decode([String].self, from: data)
             } else {
-                print("not exist")
+//                print("not exist")
             }
         }
         return [String]()
@@ -42,15 +42,15 @@ struct Helper {
     static func getImageFromUserDefaults(key: String) -> UIImage {
         print("here")
         if let userDefaults = UserDefaults(suiteName: appGroupName) {
-            print("userDefaults2:", userDefaults)
+//            print("userDefaults2:", userDefaults)
 //            print("userDefaults data3:", userDefaults.object(forKey: "29F53808-0302-43BD-AEF6-420A6482B226"))
 //
             if let imageData = userDefaults.object(forKey: key) as? Data,
                
                 let image = UIImage(data: imageData) {
-                print("imageData:", imageData)
-                print("image:", image)
-                print("** userDefaults should appear:", userDefaults.data(forKey: userDefaultsPhotosKey)!)
+//                print("imageData:", imageData)
+//                print("image:", image)
+//                print("** userDefaults should appear:", userDefaults.data(forKey: userDefaultsPhotosKey)!)
                 return image
             }
         }
