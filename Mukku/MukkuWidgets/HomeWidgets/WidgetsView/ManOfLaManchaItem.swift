1d035327b671b48130a73339f2975ffcecc3a2fd
dmajorcode <dianekang93@gmail.com>
2023-05-16 17:31:26 +0900

    🚚 chore : UIImage 함수 위치 변경

--- Mukku/MukkuWidgets/HomeWidgets/WidgetsView/ManOfLaManchaItem.swift
+++ Mukku/MukkuWidgets/HomeWidgets/WidgetsView/ManOfLaManchaItem.swift
@@ -1,16 +1,6 @@
 import SwiftUI
 import WidgetKit
 
-extension UIImage {
-  func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
-    let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
-    let format = imageRendererFormat
-    format.opaque = isOpaque
-    return UIGraphicsImageRenderer(size: canvas, format: format).image {
-      _ in draw(in: CGRect(origin: .zero, size: canvas))
-    }
-  }
-}
 
 struct ManOfLaManchaItem: View {
     
