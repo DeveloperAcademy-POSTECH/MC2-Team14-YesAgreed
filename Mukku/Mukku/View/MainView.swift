4677dc2652b390bd6faec24dd62e6c6b63e00733
dmajorcode <dianekang93@gmail.com>
2023-05-16 17:32:48 +0900

    ✏️ edit : Section 수정

--- Mukku/Mukku/View/MainView.swift
+++ Mukku/Mukku/View/MainView.swift
@@ -29,107 +29,108 @@ struct MainView: View {
     
     var body: some View {
         NavigationStack{
-            Form {
-                Section (header:
-                            VStack {
-                    ForEach(MusicalModel.musicalModels, id:\.self){ musical in
-                        Text(musical.title)
-                            .bold()
-                            .foregroundColor(.black)
-                            .font(.system(size: 23))
-                            .textCase(nil)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        LazyVGrid(columns: columns) {
-                            ForEach(musical.scene, id:\.self) { scene in
-                                Button {
-                                    feedbackGenerator.impactOccurred()
-                                    dynamicIslandScene = musical.title + scene.name
-                                    selectedScene = scene.name.lowercased()
-                                    // Dynamic island update
-                                    let updatedState = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
-                                    let updatedContent = ActivityContent(state: updatedState, staleDate: .now.advanced(by: 1800.0))
-                                    Task {
-                                        await activity?.update(updatedContent)
-                                    }
-                                } label: {
-                                    ObjectView(isSelected: dynamicIslandScene == musical.title + scene.name, text: scene.name, imageName: scene.icon)}
-                                .animation(nil, value: self.dynamicIslandScene)
-                                .padding(.bottom, 20)
+            ZStack{
+                Form {
+                    Section (header:
+                                VStack {
+                        ForEach(MusicalModel.musicalModels, id:\.self){ musical in
+                            Text(musical.title)
+                                .bold()
+                                .foregroundColor(.black)
+                                .font(.system(size: 23))
+                                .textCase(nil)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            
+                            LazyVGrid(columns: columns) {
+                                ForEach(musical.scene, id:\.self) { scene in
+                                    Button {
+                                        feedbackGenerator.impactOccurred()
+                                        dynamicIslandScene = musical.title + scene.name
+                                        selectedScene = scene.name.lowercased()
+                                        // Dynamic island update
+                                        let updatedState = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
+                                        let updatedContent = ActivityContent(state: updatedState, staleDate: .now.advanced(by: 1800.0))
+                                        Task {
+                                            await activity?.update(updatedContent)
+                                        }
+                                    } label: {
+                                        ObjectView(isSelected: dynamicIslandScene == musical.title + scene.name, text: scene.name, imageName: scene.icon)}
+                                    .animation(nil, value: self.dynamicIslandScene)
+                                    .padding(.bottom, 20)
+                                }
                             }
                         }
                     }
-                }
-                    .frame(width: screenWidth * 0.85) // 그리드 폭 조절
-                    .padding(.bottom, -20)
-                ){
-                }
-                
-                Section(header: Text("Dynamic Island Setting")) {
-                    Toggle(
-                        "Dynamic Island",
-                        isOn: $isTrackingTime).onChange(of:isTrackingTime) { _ in
-                            if isTrackingTime {
-                                startTime = .now
-                                //Start the live Activity
-                                let attributes = MukkuWidgetsAttributes()
-                                let state = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
-                                let content =
-                                ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
-                                
-                                activity = try? Activity<MukkuWidgetsAttributes>.request(
-                                    attributes: attributes,
-                                    content: content )
-                            } else {
-                                //End the live Activity
-                                guard let startTime else{return}
-                                let state = MukkuWidgetsAttributes.ContentState(startTime: startTime, scene: selectedScene)
-                                let content = ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
-                                Task{await activity?.end(content, dismissalPolicy: .immediate)}
-                                self.startTime = nil
+                        .frame(width: screenWidth * 0.85) // 그리드 폭 조절
+                        .padding(.bottom, -20)
+                    ){
+                    }
+                    Section(header: Text("Dynamic Island Setting")) {
+                        Toggle(
+                            "Dynamic Island",
+                            isOn: $isTrackingTime).onChange(of:isTrackingTime) { _ in
+                                if isTrackingTime {
+                                    startTime = .now
+                                    //Start the live Activity
+                                    let attributes = MukkuWidgetsAttributes()
+                                    let state = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
+                                    let content =
+                                    ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
+                                    
+                                    activity = try? Activity<MukkuWidgetsAttributes>.request(
+                                        attributes: attributes,
+                                        content: content )
+                                } else {
+                                    //End the live Activity
+                                    guard let startTime else{return}
+                                    let state = MukkuWidgetsAttributes.ContentState(startTime: startTime, scene: selectedScene)
+                                    let content = ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
+                                    Task{await activity?.end(content, dismissalPolicy: .immediate)}
+                                    self.startTime = nil
+                                }
                             }
-                        }
+                    }
+                    Section(header: Text("Instruction")) {
+                        NavigationLink(destination: InstructionView(instruction: "Widget")){Text(Image(systemName: "info.circle")).foregroundColor(.blue)+Text(" Widget").foregroundColor(.blue)}
+                        NavigationLink(destination: InstructionView(instruction: "Dynamic Island")){Text(Image(systemName: "info.circle")).foregroundColor(.blue)+Text(" Dynamic Island").foregroundColor(.blue)}
+                        NavigationLink(destination: InstructionView(instruction: "Watch")){Text(Image(systemName: "info.circle")).foregroundColor(.blue)+Text(" Watch").foregroundColor(.blue)}
+                    }
+                    
+                    Section(header: Text("Customization")) {
+                        NavigationLink(destination: BackgroundView()){Text("Transparent Widget Background")}
+                    }
+
+                    // 위젯 설정 방법
+                    //                    WidgetInstruction()
                 }
-            }
-            Section(header: Text("Instruction")) {
-                NavigationLink(destination: InstructionView(instruction: "Widget")){Text(Image(systemName: "info.circle")).foregroundColor(.blue)+Text(" Widget").foregroundColor(.blue)}
-                NavigationLink(destination: InstructionView(instruction: "Dynamic Island")){Text(Image(systemName: "info.circle")).foregroundColor(.blue)+Text(" Dynamic Island").foregroundColor(.blue)}
-                NavigationLink(destination: InstructionView(instruction: "Watch")){Text(Image(systemName: "info.circle")).foregroundColor(.blue)+Text(" Watch").foregroundColor(.blue)}
-            }
-            
-            Section(header: Text("Customization")) {
-                NavigationLink(destination: BackgroundView()){Text("Transparent Widget Background")}
-            }
-            
-            // Dynamic Object 애니메이션
-            switch dynamicIslandScene {
-            case let scene where scene.contains("Mask"):
-                MaskView()
-            case let scene where scene.contains("Chandelier"):
-                if UIDevice.current.name == "iPhone 14 Pro Max" {
+                // Dynamic Object 애니메이션
+                switch dynamicIslandScene {
+                case let scene where scene.contains("Mask"):
+                    MaskView()
+                case let scene where scene.contains("Chandelier"):
+                    if UIDevice.current.name == "iPhone 14 Pro Max" {
                         ChandelierView()
                             .offset(y: -42)
                     } else {
                         ChandelierView()
                     }
-            case let scene where scene.contains("Boat"):
-                BoatView()
-            case let scene where scene.contains("Rose"):
-                RoseView()
-            case let scene where scene.contains("Sunflower"):
-                SunFlowerView()
-            case let scene where scene.contains("Rapier"):
-                RapierView()
-            case let scene where scene.contains("Windmill"):
-                WindMillView()
-            case let scene where scene.contains("Typo"):
-                TypoView()
-            default:
-                EmptyView()
-            }
+                case let scene where scene.contains("Boat"):
+                    BoatView()
+                case let scene where scene.contains("Rose"):
+                    RoseView()
+                case let scene where scene.contains("Sunflower"):
+                    SunFlowerView()
+                case let scene where scene.contains("Rapier"):
+                    RapierView()
+                case let scene where scene.contains("Windmill"):
+                    WindMillView()
+                case let scene where scene.contains("Typo"):
+                    TypoView()
+                default:
+                    EmptyView()
+                }
             }
         }
-        
     }
 }
 
