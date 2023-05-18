a029bbdc6f7d59d87c07052be06ca335002a47e7
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-18 21:42:50 +0900

    🐛 fix : 강제종료 시 다이나믹 아일랜드 동작 안하는 버그 픽스

--- Mukku/Mukku/MukkuApp.swift
+++ Mukku/Mukku/MukkuApp.swift
@@ -1,10 +1,33 @@
 import SwiftUI
-
+import ActivityKit
 @main
 struct MukkuApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
-            MainView().environmentObject(ImageManager())
+            VStack{
+                ForEach(Activity<MukkuWidgetsAttributes>.activities){ hmhm in
+                    Text(String(describing: hmhm.content.state.scene))
+                }
+                MainView().environmentObject(ImageManager())
+            }
+        }.onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .background:
+                // 앱이 백그라운드로 전환될 때 실행할 코드
+                print("앱이 백그라운드로 전환됨")
+                // 앱 종료 또는 필요한 정리 작업 수행
+                // 예: 데이터 저장, 리소스 정리 등
+            case .inactive:
+                // 앱이 비활성화될 때 실행할 코드
+                print("앱이 비활성화됨")
+            case .active:
+                // 앱이 활성화될 때 실행할 코드
+                print("앱이 활성화됨")
+            @unknown default:
+                // 알 수 없는 상태일 때 실행할 코드
+                break
+            }
         }
     }
 }
--- Mukku/Mukku/View/MainView.swift
+++ Mukku/Mukku/View/MainView.swift
@@ -9,8 +9,9 @@ extension UIScreen{
 private var twoColumnsGrid = [GridItem(.flexible()), GridItem(.flexible())]
 
 struct MainView: View {
+    @State private var isAppLaunchedFirstTime : Bool = true
     @State private var isTrackingTime : Bool = false
-    @State private var activity: Activity<MukkuWidgetsAttributes>? = nil
+    @State private var activity: Activity<MukkuWidgetsAttributes>? = Activity<MukkuWidgetsAttributes>.activities.first
     @State private var startTime: Date? = nil
     @State var dynamicEnabled: Bool = true
     @State var dynamicIslandScene: String = ""
@@ -25,6 +26,12 @@ struct MainView: View {
             ZStack{
                 Form {
                     Section (header: VStack {
+                        Button{
+                            if activity != nil {
+                                Task{await activity?.end(ActivityContent(state: MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene), staleDate: .now.advanced(by: 0.01)), dismissalPolicy: .immediate)}
+                            }} label:{
+                                Text("Press Me If There is Stock in First Time")
+                            }
                         ForEach(MusicalModel.musicalModels, id:\.self){ musical in
                             Text(LocalizedStringKey(musical.title))
                                 .bold()
@@ -40,7 +47,7 @@ struct MainView: View {
                                         selectedScene = scene.name.lowercased()
                                         // Dynamic island update
                                         let updatedState = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
-                                        let updatedContent = ActivityContent(state: updatedState, staleDate: .now.advanced(by: 1800.0))
+                                        let updatedContent = ActivityContent(state: updatedState, staleDate: .now.advanced(by: 0.01))
                                         Task {
                                             await activity?.update(updatedContent)
                                         }
@@ -68,20 +75,27 @@ struct MainView: View {
                     isOn: $isTrackingTime).onChange(of:isTrackingTime) { _ in
                         if isTrackingTime {
                             startTime = .now
-                            //Start the live Activity
-                            let attributes = MukkuWidgetsAttributes()
-                            let state = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
-                            let content =
-                            ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
-                                    
-                                    activity = try? Activity<MukkuWidgetsAttributes>.request(
-                                        attributes: attributes,
-                                        content: content )
+                            Task{
+                                //강제종료로 인한 에러 캐치및 처리
+                                if activity != nil && isAppLaunchedFirstTime{
+                                    await activity?.end(ActivityContent(state: MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene), staleDate: .now.advanced(by: 0.01)), dismissalPolicy: .immediate)
+                                    isAppLaunchedFirstTime = false
+                                }
+                                //Start the live Activity
+                                let attributes = MukkuWidgetsAttributes()
+                                let state = MukkuWidgetsAttributes.ContentState(startTime: .now, scene: selectedScene)
+                                let content =
+                                ActivityContent(state: state, staleDate: .now.advanced(by: 0.01))
+                                        
+                                        activity = try? Activity<MukkuWidgetsAttributes>.request(
+                                            attributes: attributes,
+                                            content: content )
+                            }
                                 } else {
                                     //End the live Activity
                                     guard let startTime else{return}
                                     let state = MukkuWidgetsAttributes.ContentState(startTime: startTime, scene: selectedScene)
-                                    let content = ActivityContent(state: state, staleDate: .now.advanced(by: 3600.0))
+                                    let content = ActivityContent(state: state, staleDate: .now.advanced(by: 0.01))
                                     Task{await activity?.end(content, dismissalPolicy: .immediate)}
                                     self.startTime = nil
                                 }
