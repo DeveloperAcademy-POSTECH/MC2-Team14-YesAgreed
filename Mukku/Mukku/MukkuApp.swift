d33980902ff36ade5a2188a0a8785366fb500b04
leeve1247 <69492686+leeve1247@users.noreply.github.com>
2023-05-18 21:49:27 +0900

    ✏️ edit : 불필요한 ActivityKit 및 @Environment  어노테이션 삭제

--- Mukku/Mukku/MukkuApp.swift
+++ Mukku/Mukku/MukkuApp.swift
@@ -1,8 +1,7 @@
 import SwiftUI
-import ActivityKit
+
 @main
 struct MukkuApp: App {
-    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
                 MainView().environmentObject(ImageManager())
